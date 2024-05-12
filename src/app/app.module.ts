91828452dd5290614954d5428604833d06688400
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-05-12 17:58:23 -0300

    🎉 Initial commit: Projeto angular de cards, by Eduardo Xavier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BalanceCardComponent } from './components/balance-card/balance-card.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
+import { DebitCardComponent } from './components/debit-card/debit-card.component';
+import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BalanceCardComponent,
+    CreditCardComponent,
+    DebitCardComponent,
+    ClientInfosCardComponent
   ],
   imports: [
     BrowserModule,
